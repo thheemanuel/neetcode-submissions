460e292109aadb53ad9eb9cfa27532093983bf9f
theopeo <138706642+thheemanuel@users.noreply.github.com>
2026-09-01 18:35:43 +0200

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+
+        map = {}
+
+        for char in s:
+            
+            map[char] = map.get(char, 0) + 1
+        
+        for char in t:
+
+            map[char] = map.get(char, 0) - 1
+
+        for v in map.values():
+            if v != 0:
+                return False
+
+        return True
+
